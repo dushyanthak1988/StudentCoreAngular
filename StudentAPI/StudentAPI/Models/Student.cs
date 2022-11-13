33098774045cce0f4bd908dda26d524d8f5abd56
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2022-11-14 03:03:50 +0530

    add Exting  files

---
+++ StudentAPI/StudentAPI/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAPI.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Class { get; set; }
+    }
+}
